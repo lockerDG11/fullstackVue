750bdb443179ebd584bfda7856b3161717033d4e
Duc Loc Nguyen <hm-nguyen28@hm.edu>
2022-03-21 17:08:34 +0100

    fix version 4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App hahahah from Masterbranch"/>
+  <HelloWorld msg="Welcome to Your Vue.js App haha master branche"/>
 </template>
 
 <script>
